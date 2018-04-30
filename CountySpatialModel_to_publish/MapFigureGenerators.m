7940418e053fa83d008d39176c0561adf350ac3f
Jeremy P D'Silva <jpdsilva@umich.edu>
2018-04-30 12:47:12 -0400

    Added KeyFile, updated Map Generator
    Added a KeyFile to explain which index corresponds to which district and which elements of the shape file correspond to which district. 
    Updated MapFigureGenerators.m to contain more helpful/explanatory comments.

--- CountySpatialModel_to_publish/MapFigureGenerators.m
+++ CountySpatialModel_to_publish/MapFigureGenerators.m
@@ -1,10 +1,17 @@
-%% Setting up for teh country plots
-shape1 = shaperead('GIN_admin_SHP/GIN.shp');
-shape2 = shaperead('SIL_admin_SHP/SIL.shp');
-shape3 = shaperead('LIB-level_1_SHP/LIB-level_1.shp');
-shape4 = shaperead('GIN_outline_SHP/GIN_outline.shp');
-shape5 = shaperead('SIL_outline_SHP/SIL_outline.shp');
-shape6 = shaperead('LIB_outline_SHP/LIB_outline.shp');
+%% Setting up for the country plots
+%% District shapes
+Guinea_Districts = shaperead('GIN_admin_SHP/GIN.shp');
+SL_Districts = shaperead('SIL_admin_SHP/SIL.shp');
+Liberia_Districts = shaperead('LIB-level_1_SHP/LIB-level_1.shp');
+
+% NOTE: To see which elements of the shape struct correspond to each
+% district, please see KeyFile.xlsx.  
+
+%% Outlines
+Guinea_Outline = shaperead('GIN_outline_SHP/GIN_outline.shp');
+SL_Outline = shaperead('SIL_outline_SHP/SIL_outline.shp');
+Liberia_Outline = shaperead('LIB_outline_SHP/LIB_outline.shp');
+
 %%%%%%%%%%%%%%%%%%%%%%
 %% IMPORTANT NOTES!! %%
 %%%%%%%%%%%%%%%%%%%%%%
@@ -68,133 +75,133 @@ for junkIndex = 1
     set(gca,'LineWidth',1,'FontSize',20,'FontName','Arial','FontWeight','Bold')
     hold on
     
-    for i = 1:length(shape4)
-        plot(shape4(i).X(1:end-1),shape4(i).Y(1:end-1),'k','LineWidth',2.7)
+    for i = 1:length(Guinea_Outline)
+        plot(Guinea_Outline(i).X(1:end-1),Guinea_Outline(i).Y(1:end-1),'k','LineWidth',2.7)
     end
     
-    for i = 1:length(shape5)
-        plot(shape5(i).X(1:end-1),shape5(i).Y(1:end-1),'k','LineWidth',2.7)
+    for i = 1:length(SL_Outline)
+        plot(SL_Outline(i).X(1:end-1),SL_Outline(i).Y(1:end-1),'k','LineWidth',2.7)
     end
     
-    for i = 1:length(shape6)
-        plot(shape6(i).X(1:end-1),shape6(i).Y(1:end-1),'k','LineWidth',2.7)
+    for i = 1:length(Liberia_Outline)
+        plot(Liberia_Outline(i).X(1:end-1),Liberia_Outline(i).Y(1:end-1),'k','LineWidth',2.7)
     end
     
-    for i = 1:length(shape1)
-        plot(shape1(i).X(1:end-1),shape1(i).Y(1:end-1),'k','LineWidth',1.35)
+    for i = 1:length(Guinea_Districts)
+        plot(Guinea_Districts(i).X(1:end-1),Guinea_Districts(i).Y(1:end-1),'k','LineWidth',1.35)
     end
     
-    for i = 1:length(shape2)
-        plot(shape2(i).X(1:end-1),shape2(i).Y(1:end-1),'k','LineWidth',1.35)
+    for i = 1:length(SL_Districts)
+        plot(SL_Districts(i).X(1:end-1),SL_Districts(i).Y(1:end-1),'k','LineWidth',1.35)
     end
     
-    for i = 1:length(shape3)
-        plot(shape3(i).X(1:end-1),shape3(i).Y(1:end-1),'k','LineWidth',1.35)
+    for i = 1:length(Liberia_Districts)
+        plot(Liberia_Districts(i).X(1:end-1),Liberia_Districts(i).Y(1:end-1),'k','LineWidth',1.35)
     end
     
     for i = 1:3
-        fill(shape1(i).X(1:end-1),shape1(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,2))
+        fill(Guinea_Districts(i).X(1:end-1),Guinea_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,2))
     end
     for i = 4:13
-        fill(shape1(i).X(1:end-1),shape1(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,3))
+        fill(Guinea_Districts(i).X(1:end-1),Guinea_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,3))
     end
     
-    fill(shape1(14).X(1:end-1),shape1(14).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,11))
-    fill(shape1(15).X(1:end-1),shape1(15).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,13))
+    fill(Guinea_Districts(14).X(1:end-1),Guinea_Districts(14).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,11))
+    fill(Guinea_Districts(15).X(1:end-1),Guinea_Districts(15).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,13))
     
     for i = 16:22
-        fill(shape1(i).X(1:end-1),shape1(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,1))
+        fill(Guinea_Districts(i).X(1:end-1),Guinea_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,1))
     end
     
     for i = 23:25
-        fill(shape1(i).X(1:end-1),shape1(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 3)))
+        fill(Guinea_Districts(i).X(1:end-1),Guinea_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 3)))
     end
     
-    fill(shape1(26).X(1:end-1),shape1(26).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,30))
+    fill(Guinea_Districts(26).X(1:end-1),Guinea_Districts(26).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,30))
     
     for i = 27:31
-        fill(shape1(i).X(1:end-1),shape1(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 4)))
+        fill(Guinea_Districts(i).X(1:end-1),Guinea_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 4)))
     end
     
-    fill(shape1(32).X(1:end-1),shape1(32).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,4))
+    fill(Guinea_Districts(32).X(1:end-1),Guinea_Districts(32).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,4))
     
     for i = 33:36
-        fill(shape1(i).X(1:end-1),shape1(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,5))
+        fill(Guinea_Districts(i).X(1:end-1),Guinea_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,5))
     end
     
     for i = 37:39
-        fill(shape1(i).X(1:end-1),shape1(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,6))
+        fill(Guinea_Districts(i).X(1:end-1),Guinea_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,6))
     end
     
     for i = 40:42
-        fill(shape1(i).X(1:end-1),shape1(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 33)))
+        fill(Guinea_Districts(i).X(1:end-1),Guinea_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 33)))
     end
     
-    fill(shape1(43).X(1:end-1),shape1(43).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,12))
+    fill(Guinea_Districts(43).X(1:end-1),Guinea_Districts(43).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,12))
     
     for i = 44:46
-        fill(shape1(i).X(1:end-1),shape1(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 30)))
+        fill(Guinea_Districts(i).X(1:end-1),Guinea_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 30)))
     end
     
-    fill(shape1(47).X(1:end-1),shape1(47).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,19))
-    fill(shape1(48).X(1:end-1),shape1(48).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,10))
+    fill(Guinea_Districts(47).X(1:end-1),Guinea_Districts(47).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,19))
+    fill(Guinea_Districts(48).X(1:end-1),Guinea_Districts(48).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,10))
     
     for i = 49:50
-        fill(shape1(i).X(1:end-1),shape1(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 32)))
+        fill(Guinea_Districts(i).X(1:end-1),Guinea_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 32)))
     end
     for i = 51:52
-        fill(shape1(i).X(1:end-1),shape1(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 23)))
+        fill(Guinea_Districts(i).X(1:end-1),Guinea_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 23)))
     end
     
     for i = 53:56
-        fill(shape1(i).X(1:end-1),shape1(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 22)))
+        fill(Guinea_Districts(i).X(1:end-1),Guinea_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i - 22)))
     end
     
     
     for i = 1:4
-        fill(shape2(i).X(1:end-1),shape2(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i + 34)))
+        fill(SL_Districts(i).X(1:end-1),SL_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i + 34)))
     end
     
     for i = 5:7
-        fill(shape2(i).X(1:end-1),shape2(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,39))
+        fill(SL_Districts(i).X(1:end-1),SL_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,39))
     end
     
-    fill(shape2(8).X(1:end-1),shape2(8).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,40))
+    fill(SL_Districts(8).X(1:end-1),SL_Districts(8).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,40))
     
     for i = 9:17
-        fill(shape2(i).X(1:end-1),shape2(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,41))
+        fill(SL_Districts(i).X(1:end-1),SL_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,41))
     end
     
     for i = 18:19
-        fill(shape2(i).X(1:end-1),shape2(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i + 24)))
+        fill(SL_Districts(i).X(1:end-1),SL_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,(i + 24)))
     end
     
     for i = 20:37
-        fill(shape2(i).X(1:end-1),shape2(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,44))
+        fill(SL_Districts(i).X(1:end-1),SL_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,44))
     end
     
-    fill(shape2(38).X(1:end-1),shape2(38).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,45))
+    fill(SL_Districts(38).X(1:end-1),SL_Districts(38).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,45))
     
     for i = 39:41
-        fill(shape2(i).X(1:end-1),shape2(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,46))
+        fill(SL_Districts(i).X(1:end-1),SL_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,46))
     end
     
     for i = 42:43
-        fill(shape2(i).X(1:end-1),shape2(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,47))
+        fill(SL_Districts(i).X(1:end-1),SL_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,47))
     end
     
-    fill(shape2(44).X(1:end-1),shape2(44).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,48))
+    fill(SL_Districts(44).X(1:end-1),SL_Districts(44).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,48))
     
     
     
-    fill(shape3(1).X(1:end-1),shape3(1).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,49))
-    fill(shape3(2).X(1:end-1),shape3(2).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,50))
+    fill(Liberia_Districts(1).X(1:end-1),Liberia_Districts(1).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,49))
+    fill(Liberia_Districts(2).X(1:end-1),Liberia_Districts(2).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,50))
     for i = 3:12
-        fill(shape3(i).X(1:end-1),shape3(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,i + 49))
+        fill(Liberia_Districts(i).X(1:end-1),Liberia_Districts(i).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,i + 49))
     end
-    fill(shape3(13).X(1:end-1),shape3(13).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,63))
-    fill(shape3(14).X(1:end-1),shape3(14).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,62))
-    fill(shape3(15).X(1:end-1),shape3(15).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,51))
+    fill(Liberia_Districts(13).X(1:end-1),Liberia_Districts(13).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,63))
+    fill(Liberia_Districts(14).X(1:end-1),Liberia_Districts(14).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,62))
+    fill(Liberia_Districts(15).X(1:end-1),Liberia_Districts(15).Y(1:end-1),'r','FaceAlpha',rCases(plotDate,51))
     diffValues = zeros(1,63);
     diffValue = 0;
 end
